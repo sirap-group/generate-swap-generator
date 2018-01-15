40ef5ee1ef09272d4ca3fe74fec17129a4fda542
Rémi Becheras <rbecheras@sirap.fr>
2018-01-16 00:01:50 +0100

    create task libs-utils-helpers and queue it to the libs task in swap-generator-main subgenerator template

--- src/lib/subgenerators/generate-swap-generator-main/generator.js
+++ src/lib/subgenerators/generate-swap-generator-main/generator.js
@@ -126,7 +126,8 @@ export default function (app) {
    * @name lib
    * @api public
    */
-  app.task('libs', ['libs-utils-utils'])
+  app.task('libs', ['libs-utils-helpers', 'libs-utils-utils'])
+  task(app, 'libs-utils-helpers', 'generate-swap-generator-main/libs-utils-helpers.js')
   task(app, 'libs-utils-utils', 'generate-swap-generator-main/libs-utils-utils.js')
 
   /**
