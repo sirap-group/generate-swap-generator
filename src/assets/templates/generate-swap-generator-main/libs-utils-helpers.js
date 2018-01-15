97a95060012906b805eb04dcadbc61e087e151f7
Rémi Becheras <rbecheras@sirap.fr>
2018-01-15 21:12:01 +0100

    create libs-utils-helpers.js module template

---
+++ src/assets/templates/generate-swap-generator-main/libs-utils-helpers.js
@@ -0,0 +1,16 @@
+---
+layout: false
+rename:
+  basename: 'src/lib/utils/helpers.js'
+---
+export { escapeQuotes, camelcase }
+
+function escapeQuotes (str) {
+  return str.replace(/\\?"/g, '\\"')
+}
+
+function camelcase (str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => {
+    return index === 0 ? letter.toLowerCase() : letter.toUpperCase()
+  }).replace(/\s+/g, '')
+}
